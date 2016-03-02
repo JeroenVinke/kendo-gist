32538620688b7fb0e30b5d360ecdcd5849db4aa1
Jeroen <Jeroenvinke17@gmail.com>
2016-03-02 18:09:30 +0100

    no message

--- config.kendo.js
+++ config.kendo.js
@@ -1,5 +1,6 @@
 System.config({
   paths: {
-    "kendo-ui/*": "https://kendo.cdn.telerik.com/2016.1.226/*"
+    "kendo-ui/*": "https://kendo.cdn.telerik.com/2016.1.226/*",
+    "main": "https://cdn.rawgit.com/JeroenVinke/kendo-gist/master/main.js"
   }
 });
