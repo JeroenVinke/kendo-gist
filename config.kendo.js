e48b32f071094c232b2a819867b524b8ffba853d
Jeroen <Jeroenvinke17@gmail.com>
2016-03-02 18:05:58 +0100

    init

---
+++ config.kendo.js
@@ -0,0 +1,5 @@
+System.config({
+  paths: {
+    "kendo-ui/*": "https://kendo.cdn.telerik.com/2016.1.226/*"
+  }
+});
---
+++ main.js
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * The following two lines enable async/await without using babel's
+ * "runtime" transformer. Uncomment the lines if you intend to use async/await.
+ * 
+ * More info here:  https://github.com/jdanyow/aurelia-plunker/issues/2
+ */
+//import regeneratorRuntime from 'babel-runtime/regenerator';
+//window.regeneratorRuntime = regeneratorRuntime;
+/******************************************************************************/
+
+export function configure(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .developmentLogging();
+
+  aurelia.start().then(a => a.setRoot());
+}
